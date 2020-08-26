4bd8c83eddec5dc1dd48a38d5361afbe5e4dc442
jamesjUVA <64095036+jamesjUVA@users.noreply.github.com>
2020-08-26 17:28:25 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 live=True
 
-abnormalOptions=['normal','mass','ulcer','bleeding','angioectasia','edemous villi','polyp','inflammation','chylous_cyst', 'clot','varicosity','other']
+abnormalOptions=['normal','mass','ulcer','bleeding','angioectasia','edemous_villi','polyp','erosion','inflammation','chylous_cyst', 'clot','varicosity','other']
 sectOptions=['pre-stomach','stomach', 'small bowel', 'colon']
 
 frames=[-3,-2,-1,0,1,2,3]
